450bd83d5d7ebd1f402f015466a669ab9bf39f52
radekBednarik <radek.bednarik@tesena.com>
2024-04-29 09:12:03 +0200

    dev: fixes

--- stf/run.sh
+++ stf/run.sh
@@ -2,13 +2,18 @@
 
 echo "Node.js version:"
 node --version
+
 echo "NPM version:"
 npm --version
+
 echo "Switching to rethinkdb_data folder"
 cd rethinkdb_data 
+
 echo "Starting rethinkdb"
-rethinkdb & 
+rethinkdb > /dev/stdout 2>&1 & 
+
 echo "Starting adb server"
-adb start-server & 
+adb start-server > /dev/stdout 2>&1 &
+
 echo "starting stf local"
 stf local --public-ip 127.0.0.1
